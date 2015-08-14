524fe22f2847bf19f53601172ee64cb8e8baba7d
Kudaiar Pirimbaev <kudayar.pirimbaev@gmail.com>
2015-08-14 13:58:16 +0600

    Create test.cpp

---
+++ cplusplus/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int input;
+		cin >> input;
+		if (input == 42)
+		{
+			break;
+		}
+		cout << input << endl;
+	}
+	return 0;
+}
